54399aa0d002ea8006c35d42263d07ab8950d760
WXJ <18772118541@163.com>
2021-04-26 12:28:10 +0800

    commit new config

--- wubing-server/src/main/java/com/wxj/application/ServerApplication.java
+++ wubing-server/src/main/java/com/wxj/application/ServerApplication.java
@@ -4,17 +4,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**  
-* @ClassName: ServerApplication  
-* @Description: TODO 服务总线
-* @author: wxj  
-* @date: 2018-09-12 11:14
-* @Tel:18772118541
-* @email:18772118541@163.com
-*/
+/**
+ * @ClassName: ServerApplication
+ * @Description: TODO 服务总线
+ * @author: wxj
+ * @date: 2018-09-12 11:14
+ * @Tel:18772118541
+ * @email:18772118541@163.com
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class ServerApplication {
+	// skafhsdkh
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ServerApplication.class).run(args);
 	}
